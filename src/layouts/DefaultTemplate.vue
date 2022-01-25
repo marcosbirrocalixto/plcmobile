b0986aad7bf48ec18b0b2a45e872727a94a80ae7
Marcos Birro Calixto <58310513+marcosbirrocalixto@users.noreply.github.com>
2022-01-25 13:45:48 -0300

    First Commit

---
+++ src/layouts/DefaultTemplate.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        sdfsdfsdf
+    </div>
+</template>
+
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default {
+    
+}
+</script>
+
+<style scoped>
+@import url('../assets/css/plcmobile.css');
+@import url('../assets/css/reset.css');
+
+</style>
